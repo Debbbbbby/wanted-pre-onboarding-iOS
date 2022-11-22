1bbc3e5b74240573a8f0b4e11a5df73631f5437c
Debby <debby_@kakao.com>
2022-11-22 15:38:50 +0900

    ✨ 학생추가 기능

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -9,6 +9,9 @@ import Foundation
 
 var isRun:Bool = true // 프로그램 실행 상태
 
+var studentInfo = [String:[[String]]]() // [학생이름:[[과목1,성적],[과목2,성적]]
+
+// MARK: 전체 메뉴
 while isRun {
     print("""
           원하는 기능을 입력해주세요
@@ -19,7 +22,15 @@ while isRun {
     
     switch input {
     case "1":
-        print("학생추가")
+        print("추가할 할생의 이름을 입력해주세요.")
+        let name = readLine() ?? ""
+        if (name.range(of: #"^(?:[A-Z][a-z]{1,20}){1}$"#, options: .regularExpression) != nil) {
+            insertStudent(name)
+            print(studentInfo) // [학생명: []]
+        } else {
+            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        }
+        
     case "2":
         print("학생삭제")
     case "3":
@@ -33,5 +44,17 @@ while isRun {
         isRun = false; break
     default:
         print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+    }
+}
+
+// MARK: 1. 학생추가
+func insertStudent(_ name: String) {
+    if studentInfo[name] == nil {
+        studentInfo[name] = []
+        print("\(name) 학생을 추가했습니다.")
+    } else if studentInfo[name] != nil {
+        print("\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+    } else {
+        print("입력이 잘못되었습니다. 다시 확인해주세요")
     }
 }
