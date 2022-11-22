8f9648f6225636be9be4555d5b3f23ba1b4c2154
Debby <debby_@kakao.com>
2022-11-22 15:44:01 +0900

    ✨ 학생삭제 기능

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -32,7 +32,14 @@ while isRun {
         }
         
     case "2":
-        print("학생삭제")
+        print("삭제할 할생의 이름을 입력해주세요.")
+        let name = readLine() ?? ""
+        if (name.range(of: #"^(?:[A-Z][a-z]{1,20}){1}$"#, options: .regularExpression) != nil) {
+            deleteStudent(name)
+            print(studentInfo)
+        } else {
+            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        }
     case "3":
         print("성적추가(변경)")
     case "4":
@@ -54,6 +61,18 @@ func insertStudent(_ name: String) {
         print("\(name) 학생을 추가했습니다.")
     } else if studentInfo[name] != nil {
         print("\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+    } else {
+        print("입력이 잘못되었습니다. 다시 확인해주세요")
+    }
+}
+
+// MARK: 2. 학생 삭제
+func deleteStudent(_ name: String) {
+    if studentInfo[name] != nil {
+        studentInfo[name] = nil
+        print("\(name) 학생을 삭제하였습니다.")
+    } else if studentInfo[name] == nil {
+        print("\(name) 학생을 찾지 못했습니다.")
     } else {
         print("입력이 잘못되었습니다. 다시 확인해주세요")
     }
