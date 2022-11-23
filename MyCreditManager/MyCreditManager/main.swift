ca95569fe9ac11fcd35968a541806c824ff4b058
Debby <debby_@kakao.com>
2022-11-23 14:18:42 +0900

    ✨ 성적추가 기능

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -17,13 +17,20 @@ struct Student {
 
 struct Subject {
     let title: String
-    let grade: String
+    var grade: String
 }
 
 // MARK: 전체 메뉴
 while isRun {
     
-    print(">> 현재 studentInfo \(studentInfo) \n")
+    print("\n>> 현재 studentInfo")
+    for info in studentInfo {
+        print("학생명 : \(info.name)")
+        for i in 0 ..< info.subject.count {
+            print("과목명 : \(info.subject[i].title), 성적 : \(info.subject[i].grade)")
+        }
+    }
+    print()
     print("""
           원하는 기능을 입력해주세요
           1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
@@ -56,6 +63,18 @@ while isRun {
               만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
               """)
         
+        guard let input = readLine() else { break }
+        let array = input.split(separator: " ").map{ String($0) }
+        if array.count == 3 {
+            let (name, subject, grade) = (array[0], array[1], array[2])
+            if (grade.range(of: #"^[A-DF][+-]?$"#, options: .regularExpression) != nil) {
+                updateGrade(name: name, subject: subject, grade: grade)
+            } else {
+                print("성적이 양식에 맞게 입력되지 않았습니다. 다시 시도해주세요.")
+            }
+        } else {
+            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        }
     case "4":
         print("성적삭제")
     case "5":
@@ -84,8 +103,27 @@ func deleteStudent(_ name: String) {
     if let index = studentInfo.firstIndex(where: {$0.name == name}) {
         studentInfo.remove(at: index)
         print("\(name) 학생을 삭제하였습니다.")
-    } else{
+    } else {
         print("\(name) 학생을 찾지 못했습니다.")
     }
 }
 
+// MARK: 3. 성적추가(변경)
+func updateGrade(name:String, subject: String, grade: String) {
+    // 학생 존재 여부 체크
+    if let studentIdx = studentInfo.firstIndex(where: { $0.name == name }) {
+        // 과목 존재 여부 체크
+        if let subjectIdx = studentInfo[studentIdx].subject.firstIndex(where: { $0.title == subject}) {
+            // 과목이 존재하는 경우 성적 업데이트
+            studentInfo[studentIdx].subject[subjectIdx].grade = grade
+        } else {
+            // 과목이 존재하지 않을 경우 과목, 성적 정보 추가
+            let newSubjectInfo = Subject(title: subject, grade: grade)
+            studentInfo[studentIdx].subject.append(newSubjectInfo)
+        }
+        print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경)되었습니다.")
+    } else {
+        print("\(name) 학생을 찾지 못했습니다.")
+    }
+    
+}
